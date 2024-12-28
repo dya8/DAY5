1d910a919ce7acad2e8577d4e7f67c03ac630ea4
dya8 <diyadileep0806@yahoo.com>
2024-12-28 15:36:40 +0530

    image added

--- ecommerce/src/app/ui/navbar/navbar.component.ts
+++ ecommerce/src/app/ui/navbar/navbar.component.ts
@@ -9,3 +9,39 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
 
 }
+// my-navbar.component.ts
+/*import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  menuOpen: boolean = false;
+  cartOpen: boolean = false;
+  userMenuOpen: boolean = false;
+
+  // Toggle mobile menu
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Toggle dropdowns
+  toggleDropdown(type: string) {
+    if (type === 'cart') {
+      this.cartOpen = !this.cartOpen;
+      this.userMenuOpen = false; // Close other dropdown
+    } else if (type === 'user') {
+      this.userMenuOpen = !this.userMenuOpen;
+      this.cartOpen = false; // Close other dropdown
+    }
+  }
+
+  // Remove item from cart
+  removeCartItem(itemId: number) {
+    console.log(`Removing item with ID: ${itemId}`);
+    // Logic to remove the item from your cart state
+  }
+}
+*/
